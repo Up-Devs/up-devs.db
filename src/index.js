0a3907d94932a67396c5a102b2a1ad270cf77f4c
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-08-28 14:45:21 +0300

    update(index.js): Added new classes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 module.exports = require('./base/Client.js')
 
 module.exports = {
+ Client: require('./base/Client.js'),
+ BaseClient: require('./base/BaseClient.js'),
  JsonDB: require('./base/JsonDB.js'),
  version: require('../package.json').version
 }
