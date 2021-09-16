0efe82a28254c7eb87f410c44c80c25a0ecac5f5
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-09-16 17:29:07 +0300

    Delete index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = require('./base/Client.js')
-
-module.exports = {
- Client: require('./base/Client.js'),
- BaseClient: require('./base/BaseClient.js'),
- JsonDB: require('./base/JsonDB.js'),
- version: require('../package.json').version
-}
