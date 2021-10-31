72288ec04ed67ae232e079da7e2028d0d2c608f1
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-11-01 00:05:59 +0300

    RedisDB ✨🔥 (WIP)

---
+++ src/base/RedisDB.js
@@ -0,0 +1,17 @@
+const Error = require('../Error')
+
+const EventEmitter = require('events');
+const Events = require('../manager/Events');
+
+/**
+ * Stores values in a Redis database.
+ */
+class RedisDB extends EventEmitter {
+  
+  
+  
+  
+  
+}
+
+module.exports = RedisDB;
